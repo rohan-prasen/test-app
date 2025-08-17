61d0182378731dd0b1ffa6b8017b32b0d252a1c0
21eg106a62 <21eg106a62@anurag.edu.in>
2025-08-17 13:56:26 +0000

    working change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS 
+from flask import Flask, request, jsonify, send_from_directory
 import google.generativeai as genai
 import os
 import fitz # PyMuPDF
